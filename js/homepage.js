534118e52c569a137696875208a635e117275c4a
Alex LaFroscia <alex@lafroscia.com>
2016-02-19 16:00:02 -0500

    Add dynamic time countdown to `/live` page

--- js/homepage.js
+++ js/homepage.js
@@ -1,37 +1,20 @@
-var activeQuestion, addClickHandler, mobileNavVisible;
+var activeQuestion = null;
 
-activeQuestion = null;
-
-mobileNavVisible = false;
-
-addClickHandler = function(el) {
+function addClickHandler(el) {
   return el.addEventListener('click', function() {
     activeQuestion.classList.remove('active');
     activeQuestion = this.parentElement;
     return activeQuestion.classList.add('active');
   });
-};
+}
 
-window.onload = function() {
-  var header, i, len, menu, mobileNavButton, questionHeaders, questions;
-  questions = document.querySelectorAll('.question');
+window.addEventListener("DOMContentLoaded", function() {
+  var questions = document.querySelectorAll('.question');
   activeQuestion = questions[0];
   activeQuestion.classList.add('active');
-  questionHeaders = document.querySelectorAll('.question__header');
-  for (i = 0, len = questionHeaders.length; i < len; i++) {
-    header = questionHeaders[i];
+  var questionHeaders = document.querySelectorAll('.question__header');
+  for (var i = 0, len = questionHeaders.length; i < len; i++) {
+    var header = questionHeaders[i];
     addClickHandler(header);
   }
-  mobileNavButton = document.getElementById('mobile-nav-trigger');
-  menu = document.getElementById('menu');
-  return mobileNavButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    if (!mobileNavVisible) {
-      menu.classList.add('visible');
-    }
-    if (mobileNavVisible) {
-      menu.classList.remove('visible');
-    }
-    return mobileNavVisible = !mobileNavVisible;
-  });
-};
+});
