ebfbb663cc5c49ee37ea079aa236b0439f2a8e0d
akrishan11 <118651004+akrishan11@users.noreply.github.com>
2024-07-24 01:38:16 -0400

    Revert "Steelhacks XI Website"

--- js/homepage.js
+++ js/homepage.js
@@ -1,25 +1,25 @@
 var activeQuestion = null;
 
 function addClickHandler(el) {
-  return el.addEventListener("click", function () {
-    if (activeQuestion) {
-      activeQuestion.classList.remove("active");
+  return el.addEventListener('click', function() {
+    if(activeQuestion) {
+      activeQuestion.classList.remove('active');
     }
 
     if (activeQuestion === this.parentElement) {
       activeQuestion = null;
     } else {
       activeQuestion = this.parentElement;
-      activeQuestion.classList.add("active");
+      activeQuestion.classList.add('active');
     }
   });
 }
 
-window.addEventListener("DOMContentLoaded", function () {
-  var questions = document.querySelectorAll(".question");
+window.addEventListener("DOMContentLoaded", function() {
+  var questions = document.querySelectorAll('.question');
   activeQuestion = questions[0];
-  // activeQuestion.classList.add('active');
-  var questionHeaders = document.querySelectorAll(".question__header");
+  activeQuestion.classList.add('active');
+  var questionHeaders = document.querySelectorAll('.question__header');
   for (var i = 0, len = questionHeaders.length; i < len; i++) {
     var header = questionHeaders[i];
     addClickHandler(header);
