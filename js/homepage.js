279bbcbe5bc7738a2f74e752f23decfd1c5c43dd
akrishan11 <akashkrishan11@gmail.com>
2024-09-29 13:40:39 -0400

    timer ot zero

--- js/homepage.js
+++ js/homepage.js
@@ -37,6 +37,13 @@ var x = setInterval(function () {
   var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
+  if (difference < 0) {
+    days = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+  }
+
   console.log(days);
   document.getElementById("countdown-hours").innerHTML = hours;
   document.getElementById("countdown-minutes").innerHTML = minutes;
